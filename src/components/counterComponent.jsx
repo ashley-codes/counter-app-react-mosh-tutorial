be94683c828a0df432de90a30cd4043773953e80
Ashley Ann <28735338+aglime@users.noreply.github.com>
2018-11-23 14:21:07 -0500

    adding in a parent app class and additional button functionality

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -1,31 +1,31 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value,
-    tags: []
-  };
+  // state = {
+  //   value: this.props.counter.value,
+  //   tags: []
+  // };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+  // renderTags() {
+  //   if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
+  //   return (
+  //     <ul>
+  //       {this.state.tags.map(tag => (
+  //         <li key={tag}>{tag}</li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
 
   //   constructor() {
   //     super();
   //     this.handleIncrement = this.handleIncrement.bind(this);
   //   }
 
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
+  // handleIncrement = () => {
+  //   this.setState({ value: this.state.value + 1 });
+  // };
 
   render() {
     // console.log("props", this.props);
@@ -34,10 +34,16 @@ class Counter extends Component {
         {/* {this.props.children} */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
@@ -45,12 +51,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,20 +2,30 @@ import React, { Component } from "react";
 import Counter from "./counterComponent";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 }
-    ]
-  };
   render() {
     return (
       <div>
-        {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value}>
+        <button
+          onClick={this.props.onReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
+        {/* Another reference to the props that are pass from App */}
+        {this.props.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            // bubbling up the event to the Parent App via inclusion of the props references to each of these events in App
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
+            counter={counter}
+            // value={counter.value}
+            // id={counter.id}
+          >
             {/* <h4>Counter # {counter.id}</h4> */}
+            {/* The handleDelete method is passed as a "props" so it can be access by the Counter component. */}
+            {/* The key attribute is used internally by React - so we won't be able to access it in our component - thus the additional id prop */}
+            {/* instead of passing the value and id properties - we can use the counter object itself */}
           </Counter>
         ))}
       </div>
